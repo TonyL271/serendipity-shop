d2457edbf8803867ac81dc4c3b9c6aca9c2b2f8d
Tony Liang <tony@Tonys-MacBook-Pro.local>
2022-10-02 19:28:25 -0700

    Added custom breakpoints to theme

--- src/theme.js
+++ src/theme.js
@@ -30,6 +30,14 @@ const theme = createTheme({
       `,
     }
   },
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1200,
+    },
+  },
 });
 
 export default theme;
