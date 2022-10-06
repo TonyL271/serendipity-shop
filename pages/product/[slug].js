f5fe8bd487ee23280dc197824c24cd907fc088bb
Tony Liang <tony@Tonys-MacBook-Pro.local>
2022-10-05 18:23:37 -0700

    functional product page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 import client, { urlFor } from '../../lib/client'
-import { Box, Typography, Button, Select, MenuItem, FormControl } from '@mui/material'
+import { Box, Typography, Button, Select, MenuItem, FormControl, Rating } from '@mui/material'
 import { useState } from 'react'
 
 const Post = ({ product }) => {
@@ -9,46 +9,72 @@ const Post = ({ product }) => {
     const [quantity, setQuantity] = useState(1)
 
     return (
-        <Box sx={{ width: '100vw', height: '100vh', }}>
+        <Box sx={{ width: '100vw', height: '100vh', bgcolor: '#ecf8fc', }}>
+            <Box sx={{ display: { mobile: 'box', laptop: 'none' }, width: '100vw', height: '80px', }} />
+            <Typography py="1rem" variant="h2" align='center'>{product.title}</Typography>
             <Box
                 sx={{
+                    width: '50%',
                     display: 'flex',
-                    margin: 'auto',
+                    mx: 'auto',
                     justifyContent: 'space-between',
                 }}>
-                <Box>
-                    <Typography variant="h2">{product.title}</Typography>
+                <Box sx={{ margin: '1rem' }}>
                     <Box
                         component="img"
                         src={urlFor(product.defaultProductVariant.images[0])}
                         sx={{
                             position: 'relative',
-                            width: '500px',
+                            width: '25vw',
                             gridColumn: '1/2',
                         }}
                     />
                 </Box>
-                <Box>
-                    <Box>
-                        <Typography variant="h4">{product.body}</Typography>
-                        <Typography variant="h3" sx={{ color: '#B12704' }}>{`$${product.price}`}</Typography>
-                    </Box>
-                    <FormControl sx={{ width: '20%', }}>
-                        <Select
-                            id="quantity-select"
-                            label={'quantity'}
-                            value={quantity}
-                            onChange={(e) => setQuantity(e.target.value)}
+                <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
+                    <Box sx={{ display: 'flex', width: '100%', margin: '1rem', }}>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                width: '100%',
+                                justifyContent: 'center',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                marginRight: '1rem',
+                            }}
                         >
-                            {[1, 2, 3, 4, 5].map((num) => (
-                                <MenuItem key={num} value={num}>{num}</MenuItem>
-                            ))}
-                        </Select>
-                        <Button id="add-to-cart">Add to Cart</Button>
-                    </FormControl>
+                            <Typography variant="h3" width="100%" align='left' color='#B12704' fontSize="2.0rem" >{`$${product.price}`}</Typography>
+                            <Typography variant="p" width="100%" align='left' color='black' fontSize="1.0rem" >In stock and usually ships within 1 to 3 business days.
+                            </Typography>
+                            <Typography variant="p" width="100%" align='left' color='rgba(52, 201, 235,1)' fontSize="1.0rem" >Free shipping over $50</Typography>
+                        </Box>
+                        <Box width='100%'>
+                            <FormControl>
+                                <Box display="flex" width="50%" alignItems="center" justifyContent="space-between" marginBottom="2rem" mx="auto" >
+                                    <Typography flexShrink="1" marginRight="1rem">qty:</Typography>
+                                    <Select
+                                        sx={{ width: '4rem', margin: 'auto', flexGrow: 1 }}
+                                        id="quantity-select"
+                                        label={'quantity'}
+                                        value={quantity}
+                                        onChange={(e) => setQuantity(e.target.value)}
+                                    >
+                                        {[1, 2, 3, 4, 5].map((num) => (
+                                            <MenuItem key={num} value={num}>{num}</MenuItem>
+                                        ))}
+                                    </Select>
+                                </Box>
+                                <Button id="add-to-cart" sx={{ bgcolor: 'primary.main', color: 'primary.light', width: '15rem' }}>Add to Cart</Button>
+                            </FormControl>
+                        </Box>
+                    </Box>
+                    <Box margin='1rem'>
+                        <Typography component="legend" align="left" width="100%">No rating given</Typography>
+                        <Rating name="no-value" value={null} align="left" sx={{ width: '100%', }} />
+                        <Typography variant="h4" mb="1rem">Description</Typography>
+                        <Typography variant="p">{product.body}</Typography>
+                    </Box>
                 </Box>
             </Box>
-
         </Box>
     )
 }
@@ -66,7 +92,6 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-    console.log(slug)
     const products = await client.fetch(`*[_type == "product"]`)
     const product = products.find(product => product.slug.current === slug)
     return {
