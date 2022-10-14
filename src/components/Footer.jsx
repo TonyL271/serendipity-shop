01215c27945e43a902a046850e54ab7a23de9b9e
Tony Liang <tony@Tonys-MacBook-Pro.local>
2022-10-14 04:32:50 -0700

    Finished footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
-import { Box,Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
+import Link from 'next/link'
+import GitHubIcon from '@mui/icons-material/GitHub'
 
 const Footer = () => {
   return (
-    <Box>
-      <Typography>Design and built by Tony Liang</Typography>
+    <Box sx={{
+      width: '100vw',
+      display:'flex',
+      alignItems:'center',
+      justifyContent:'center',
+      flexDirection:'column',
+      color:'white',
+      py:'0.5rem',
+      backgroundColor:'#000000',
+    }}>
+      <Typography>Designed and built by Tony Liang</Typography>
+      <Link href="https://github.com/TonyL271/serendipity-shop">
+        <GitHubIcon />
+      </Link>
     </Box>
   )
 }
